a152ea3716b5b0fcf8bde43c8c1f5b5a7e09f33e
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-10-13 23:28:16 +0300

    using js classes

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-function insert(num) {
-    document.form.textview.value  = document.form.textview.value += num;
-}
+class calculator {
 
-function clearAll () {
-    document.form.textview.value = "";
-}
+    insert(num) {
+        document.form.textview.value = document.form.textview.value += num;
+    }
 
-function backspace () {
-    let delSign = document.form.textview.value;
-    document.form.textview.value = delSign.substring(0, delSign.length-1);
-}
+    clearAll() {
+        document.form.textview.value = "";
+    }
 
-function equal() {
-    let calcValue = document.form.textview.value;
-    if(calcValue) {
-        document.form.textview.value = eval(calcValue);
+    backspace() {
+        let delSign = document.form.textview.value;
+        document.form.textview.value = delSign.substring(0, delSign.length - 1);
+    }
+
+    equal() {
+        let calcValue = document.form.textview.value;
+        if (calcValue) {
+            document.form.textview.value = eval(calcValue);
+        }
     }
 }
+
+let calc = new calculator();
